edc72d750dc8e496ef015b4233ea5bcaaae35cd5
fdemir <frndemir@gmail.com>
2022-12-22 04:10:07 +0300

    format: add prettier and format all files

--- src/foxql.js
+++ src/foxql.js
@@ -1,105 +1,105 @@
 import foxql from "@foxql/foxql-peer";
-import * as dbConfig from './database.js';
-import sha256 from 'crypto-js/sha256';
-import nodeEvents from './events.js';
+import * as dbConfig from "./database.js";
+import sha256 from "crypto-js/sha256";
+import nodeEvents from "./events.js";
 const node = new foxql({
   maxNodeCount: 80, // max connection limit
   maxCandidateCallTime: 3000, // how long to wait for a response from a candidate node
   powPoolingTime: 2000,
-  dappAlias: 'd2a-network'
+  dappAlias: "d2a-network",
 });
 
-chrome.storage.local.get("name", function(data) {
+chrome.storage.local.get("name", function (data) {
   node.setMetaData({
-    name: data.name || 'no name'
+    name: data.name || "no name",
   });
-})
-chrome.storage.local.get("nodeAvatar", function(data) {
-  node.nodeMetaData.avatar = data.nodeAvatar 
-})
+});
+chrome.storage.local.get("nodeAvatar", function (data) {
+  node.nodeMetaData.avatar = data.nodeAvatar;
+});
 
-node.loadEvents(nodeEvents)
+node.loadEvents(nodeEvents);
 
 node.start(dbConfig);
 
-chrome.storage.local.set({ nodeId: node.nodeId }).then(() => {
- 
-});
+chrome.storage.local.set({ nodeId: node.nodeId }).then(() => {});
 
-async function save(document)
-{
+async function save(document) {
   const hash = sha256(document.url + document.content).toString();
-  document.documentKey = hash
-  const {indexedDb} = node
-  const objectStore = indexedDb.transaction('documents', 'readwrite').objectStore('documents')
-  return objectStore.add(document)
+  document.documentKey = hash;
+  const { indexedDb } = node;
+  const objectStore = indexedDb
+    .transaction("documents", "readwrite")
+    .objectStore("documents");
+  return objectStore.add(document);
 }
 
 /**
- * 
- * @param {string} documentKey 
+ *
+ * @param {string} documentKey
  */
-async function checkDocumentInLocal(documentKey)
-{
-  const {indexedDb} = node
+async function checkDocumentInLocal(documentKey) {
+  const { indexedDb } = node;
   return new Promise((resolve) => {
-    const objectStore = indexedDb.transaction('documents', 'readwrite').objectStore('documents')
-    const req = objectStore.get(documentKey)
-    req.onsuccess = (e=> {
-      resolve(e.target.result == undefined ? false : true)
-    })
-  })
+    const objectStore = indexedDb
+      .transaction("documents", "readwrite")
+      .objectStore("documents");
+    const req = objectStore.get(documentKey);
+    req.onsuccess = (e) => {
+      resolve(e.target.result == undefined ? false : true);
+    };
+  });
 }
 
 /**
- * 
- * @param {string} documentKey 
+ *
+ * @param {string} documentKey
  */
-async function deleteDocumentInLocal(documentKey)
-{
-  const {indexedDb} = node
+async function deleteDocumentInLocal(documentKey) {
+  const { indexedDb } = node;
   return new Promise((resolve) => {
-    const transaction = indexedDb.transaction('documents', 'readwrite').objectStore('documents')
-    const del = transaction.delete(documentKey)
-    del.onsuccess = (()=>{
-      resolve(true)
-    })
-  })
+    const transaction = indexedDb
+      .transaction("documents", "readwrite")
+      .objectStore("documents");
+    const del = transaction.delete(documentKey);
+    del.onsuccess = () => {
+      resolve(true);
+    };
+  });
 }
 
 /**
- * 
- * @param {string} url 
- * @returns 
+ *
+ * @param {string} url
+ * @returns
  */
-async function findDocumentInLocal(url)
-{
-    const {indexedDb} = node
-    const store = indexedDb.transaction('documents', 'readonly').objectStore('documents')
-    const cursorRequest = store.openCursor();
-    
-    let foundedDocuments = []
-    
-    return new Promise((resolve) => {
-        cursorRequest.onsuccess = (e)=> {
-            const cursor = e.target.result;
-            if(cursor) {
-                const document = cursor.value
-                if(document.url == url){
-                    foundedDocuments.push(document)
-                }
-                cursor.continue()
-            }else{
-                resolve(foundedDocuments)
-            }
-            
-        }    
-    }) 
+async function findDocumentInLocal(url) {
+  const { indexedDb } = node;
+  const store = indexedDb
+    .transaction("documents", "readonly")
+    .objectStore("documents");
+  const cursorRequest = store.openCursor();
+
+  let foundedDocuments = [];
+
+  return new Promise((resolve) => {
+    cursorRequest.onsuccess = (e) => {
+      const cursor = e.target.result;
+      if (cursor) {
+        const document = cursor.value;
+        if (document.url == url) {
+          foundedDocuments.push(document);
+        }
+        cursor.continue();
+      } else {
+        resolve(foundedDocuments);
+      }
+    };
+  });
 }
 
-window.saveDocument = save
-window.checkDocumentInDb = checkDocumentInLocal
-window.deleteInDb = deleteDocumentInLocal
-window.findDocumentInDb = findDocumentInLocal
-window.foxql = node
-
+window.saveDocument = save;
+window.checkDocumentInDb = checkDocumentInLocal;
+window.deleteInDb = deleteDocumentInLocal;
+window.findDocumentInDb = findDocumentInLocal;
+window.foxql = node;
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 
-export const nodeId = writable(null)
-export const nodeName = writable('no name')
+export const nodeId = writable(null);
+export const nodeName = writable("no name");
